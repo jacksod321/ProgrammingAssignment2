544e5b7c93164976a4dbc7bcb92e262d2e820b1c
jacksod321 <jchuang504@gmail.com>
2016-12-10 12:17:50 -0500

    Rprogramming Week3 Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## Define Objective like function initialize required field
+## such default inverse matrix is null and what setinv and getinv will do
+## and also record. the cache
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
-
+## If inverse of matrix x already been calculated
+## return value by getinv, else will call solve function
+## to calculated inverse matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
